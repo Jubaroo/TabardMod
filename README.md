da07a3ec8374173e1f12197d91efb5dfdf3794a4
Cyriack <>
2018-04-30 22:58:00 -0400

    Added debug

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 - A mod that allows all kingdom tabards to be crafted and worn by players.
 
+**Version 1.2**
+
+- Added the option to toggle the ability for player to craft. Useful to make them GM spawn only for prizes, events, etc.
+- Added optional logging for the mod.
+
 **Version 1.1**
 
-- Changed the armor.xml file to reflect the new DDS format textures
-- Added ```depend.requires=serverpacks``` to the properties file to prevent starting server without the serverpack mod installed. Thanks to [ago](https://forum.wurmonline.com/index.php?/profile/827-ago/)
+- Changed the armor.xml file to reflect the new DDS format textures.
+- Added ```depend.requires=serverpacks``` to the properties file to prevent starting server without the serverpack mod installed. Thanks to [ago](https://forum.wurmonline.com/index.php?/profile/827-ago/).
 - Added Legion of Anubis tabard. 
 
 ![Legion of Anubis Tabard](https://i.imgur.com/VJ7TvzF.png)
--- src/org/jubaroo/wurmunlimited/mods/tabards/Initiator.java
+++ src/org/jubaroo/wurmunlimited/mods/tabards/Initiator.java
@@ -5,39 +5,70 @@ import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.ItemTemplatesCreatedListener;
 import org.gotti.wurmunlimited.modloader.interfaces.ServerStartedListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
-import org.jubaroo.wurmunlimited.mods.tabards.configurator.Constants;
 import org.jubaroo.wurmunlimited.mods.tabards.items.CreationEntrys;
 import org.jubaroo.wurmunlimited.mods.tabards.items.ItemTemplateCreatorJubaroo;
 
 import java.util.Properties;
+import java.util.logging.Logger;
+
+import static org.jubaroo.wurmunlimited.mods.tabards.configurator.Constants.*;
 
 public class Initiator implements WurmServerMod, ServerStartedListener, ItemTemplatesCreatedListener, Configurable {
+    private static final Logger logger = Logger.getLogger(Initiator.class.getName() + " v1.2");
+    private static boolean  debug;
 
-    public void onItemTemplatesCreated() { if (Constants.toggleTabards) { new ItemTemplateCreatorJubaroo(); } }
+    private static void debug(String msg) {
+        if (debug) { logger.info(msg); }
+    }
 
-    public void onServerStarted() { if (Constants.tabardCrafting) { new CreationEntrys(); } }
+    public void onItemTemplatesCreated() { if (toggleTabards) { new ItemTemplateCreatorJubaroo(); } }
+
+    public void onServerStarted() { if (tabardCrafting) { new CreationEntrys(); } }
 
     public void configure(Properties properties) {
-        Constants.jennTabardId = Integer.parseInt(properties.getProperty("jennTabardId"));
-        Constants.zjenTabardId = Integer.parseInt(properties.getProperty("zjenTabardId"));
-        Constants.dreaTabardId = Integer.parseInt(properties.getProperty("dreaTabardId"));
-        Constants.pandTabardId = Integer.parseInt(properties.getProperty("pandTabardId"));
-        Constants.thecTabardId = Integer.parseInt(properties.getProperty("thecTabardId"));
-        Constants.therTabardId = Integer.parseInt(properties.getProperty("therTabardId"));
-        Constants.hotsTabardId = Integer.parseInt(properties.getProperty("hotsTabardId"));
-        Constants.yvalTabardId = Integer.parseInt(properties.getProperty("yvalTabardId"));
-        Constants.blacTabardId = Integer.parseInt(properties.getProperty("blacTabardId"));
-        Constants.ebonTabardId = Integer.parseInt(properties.getProperty("ebonTabardId"));
-        Constants.abraTabardId = Integer.parseInt(properties.getProperty("abraTabardId"));
-        Constants.molrTabardId = Integer.parseInt(properties.getProperty("molrTabardId"));
-        Constants.empiTabardId = Integer.parseInt(properties.getProperty("empiTabardId"));
-        Constants.kosTabardId  = Integer.parseInt(properties.getProperty("kosTabardId"));
-        Constants.valhTabardId = Integer.parseInt(properties.getProperty("valhTabardId"));
-        Constants.maceTabardId = Integer.parseInt(properties.getProperty("maceTabardId"));
-        Constants.wurmTabardId = Integer.parseInt(properties.getProperty("wurmTabardId"));
-        Constants.legiTabardId = Integer.parseInt(properties.getProperty("legiTabardId"));
-        Constants.tabardCrafting = Boolean.valueOf(properties.getProperty("tabardCrafting", String.valueOf(true)));
-        Constants.toggleTabards = Boolean.valueOf(properties.getProperty("toggleTabards", String.valueOf(true)));
+        debug = Boolean.valueOf(properties.getProperty("debug", String.valueOf(false)));
+        debug("debug enabled: Logging starting...");
+        debug("Jenn_Kellon ID: " + jennTabardId);
+        jennTabardId = Integer.parseInt(properties.getProperty("jennTabardId"));
+        debug("Jenn_Kellon II ID: " + zjenTabardId);
+        zjenTabardId = Integer.parseInt(properties.getProperty("zjenTabardId"));
+        debug("Dreadnought ID: " + dreaTabardId);
+        dreaTabardId = Integer.parseInt(properties.getProperty("dreaTabardId"));
+        debug("Pandemonium ID: " + pandTabardId);
+        pandTabardId = Integer.parseInt(properties.getProperty("pandTabardId"));
+        debug("Crusaders ID: " + thecTabardId);
+        thecTabardId = Integer.parseInt(properties.getProperty("thecTabardId"));
+        debug("Roman Empire ID: " + therTabardId);
+        therTabardId = Integer.parseInt(properties.getProperty("therTabardId"));
+        debug("Horde of the Summoned ID: " + hotsTabardId);
+        hotsTabardId = Integer.parseInt(properties.getProperty("hotsTabardId"));
+        debug("Valhalla II ID: " + yvalTabardId);
+        yvalTabardId = Integer.parseInt(properties.getProperty("yvalTabardId"));
+        debug("Black Legion ID: " + blacTabardId);
+        blacTabardId = Integer.parseInt(properties.getProperty("blacTabardId"));
+        debug("Ebonaura ID: " + ebonTabardId);
+        ebonTabardId = Integer.parseInt(properties.getProperty("ebonTabardId"));
+        debug("Abralon ID: " + abraTabardId);
+        abraTabardId = Integer.parseInt(properties.getProperty("abraTabardId"));
+        debug("Mol-Rehan ID: " + molrTabardId);
+        molrTabardId = Integer.parseInt(properties.getProperty("molrTabardId"));
+        debug("Empire of Mol-Rehan ID: " + empiTabardId);
+        empiTabardId = Integer.parseInt(properties.getProperty("empiTabardId"));
+        debug("Kingdom of Sol ID: " + kosTabardId);
+        kosTabardId  = Integer.parseInt(properties.getProperty("kosTabardId"));
+        debug("Valhalla ID: " + valhTabardId);
+        valhTabardId = Integer.parseInt(properties.getProperty("valhTabardId"));
+        debug("Macedon ID: " + maceTabardId);
+        maceTabardId = Integer.parseInt(properties.getProperty("maceTabardId"));
+        debug("University of Wurm ID: " + wurmTabardId);
+        wurmTabardId = Integer.parseInt(properties.getProperty("wurmTabardId"));
+        debug("Legion of Anubis ID: " + legiTabardId);
+        legiTabardId = Integer.parseInt(properties.getProperty("legiTabardId"));
+        debug("Tabard Crafting = : " + tabardCrafting);
+        tabardCrafting = Boolean.valueOf(properties.getProperty("tabardCrafting", String.valueOf(true)));
+        debug("Tabards = : " + toggleTabards);
+        toggleTabards = Boolean.valueOf(properties.getProperty("toggleTabards", String.valueOf(true)));
+
     }
 
 }
